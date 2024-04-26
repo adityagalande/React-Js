550fb2f6fe27b027cb3e4a9a5de67ae506663e7b
Aditya Galande <56816119+adityagalande@users.noreply.github.com>
2024-04-26 20:15:49 +0000

    components

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,13 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  let obj = {
+    city: "Seattle",
+    country: "USA",
+    years: 5
+  }
   return (
     <>
-    <h1 className='bg-green-400 rounded-xl p-10'>Tailwind test</h1>
+      <h1 className='bg-green-400 rounded-xl p-10 mb-4'>Tailwind test</h1>
+      <Card userName="Aditya" myObj={obj} btnText="Click me"/>
+      <Card userName="Nisha" btnText="Visit me"/>
+      <Card/>
     </>
   )
 }
