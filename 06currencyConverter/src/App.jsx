bb2f47c7e2618f46fe89e8fce0a47fa959e91e0c
Aditya Galande <56816119+adityagalande@users.noreply.github.com>
2024-05-18 19:56:44 +0000

    currencyConverter

---
+++ 06currencyConverter/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='text-white font-bold bg-orange-500 w-auto text-center'>USA & CANADA I am Coming to you in fall 2025 for usa & after 3 years to canada</h1>
+    </>
+  )
+}
+
+export default App
