e63ed2a2211419b42240c9fb6f6218f93c2fd3dd
Aditya Galande <56816119+adityagalande@users.noreply.github.com>
2024-05-18 21:54:52 +0000

    commit

--- 06currencyConverter/src/App.jsx
+++ 06currencyConverter/src/App.jsx
@@ -2,13 +2,30 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import InputBox from './components/InputBox'
+import useCurrencyInfo from './custom_hooks/useCurrencyInfo'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState("usd")
+  const [to, setTo] = useState("inr")
+  const [convertedAmount, setConvertedAmount] = useState(0)
+
+  const currencyInfo = useCurrencyInfo(from)
+
+  const options = Object.keys(currencyInfo)
+
+  const swap = () => {
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
+  }
 
   return (
     <>
     <h1 className='text-white font-bold bg-orange-500 w-auto text-center'>USA & CANADA I am Coming to you in fall 2025 for usa & after 3 years to canada</h1>
+    <InputBox />
     </>
   )
 }
--- 06currencyConverter/src/custom_hooks/useCurrencyInfo.js
+++ 06currencyConverter/src/custom_hooks/useCurrencyInfo.js
@@ -1,7 +1,18 @@
 import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency){
+    const [result, setResult] = useState({});
+
     useEffect(() =>{
-        fetch(``)
-    }, [])
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`)
+        .then((res) => (res.json()))
+        .then((data) => setResult(data[currency]))
+       console.log(result)
+    }, [currency])
+
+    console.log(result)
+
+    return result;
 }
+
+export default useCurrencyInfo;
