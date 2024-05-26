71059e30c19fa827086dcc0798bc13a9cf2fdf3f
Aditya Galande <56816119+adityagalande@users.noreply.github.com>
2024-05-26 18:19:35 +0000

    hook

---
+++ 07dictionaryApp/src/custom_hook/useDictionaryApi.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+
+function useDictionaryApi(word){
+
+    const [result, setResult] = useState({})
+
+    useEffect(() => {
+
+        fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+        .then((res) => (res.json()))
+            .then((data) => (setResult(data)))
+        console.log(result)
+
+    }, [word])
+    console.log(result)
+    return result;
+
+}
+
+export default useDictionaryApi;
