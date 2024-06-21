8a22b296bb1c1d4f54f72e9f9a8b6ccda99e5261
Aditya Galande <56816119+adityagalande@users.noreply.github.com>
2024-06-21 18:12:32 +0000

    user context

--- 10miniContext/src/context/UserContext.js
+++ 10miniContext/src/context/UserContext.js
@@ -1,7 +1,7 @@
 import React, { createContext } from "react";
 
 // created variable by creating createContext mathod same like useState & exported it.
-const UserContext = React.createContext();
+const UserContext = createContext();
 
 // Here UserContext is a provider we can use it by wrapping like <div> and other component & stuff here (here these components get access of this global usercontext) </div> -> eg. <UserContext.Provider> stuff </UserContext.Provider>
 export default UserContext;
