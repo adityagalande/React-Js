e0bf5daee5411daf3d982801f8c1dc8f91a6ffcc
Aditya Galande <56816119+adityagalande@users.noreply.github.com>
2024-07-12 17:27:13 +0000

    functionality

--- 12todoContextLocalStorage/src/App.jsx
+++ 12todoContextLocalStorage/src/App.jsx
@@ -1,11 +1,37 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import './App.css'
+import { TodoProvider } from './contexts/todoContext';
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [...prev, {id: 2, ...todo}])
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? todo : prevTodo))
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((prevTodo) => prevTodo.id !== id))
+  }
+
   return(
-    <>
-      <h1 className='font-bold bg-green-200 text-4xl text-center' >USA & CANADA I'm Coming To You in Fall 2025</h1>
-    </>
+    <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
+      </div>
+    </TodoProvider>
   );
 }
 
