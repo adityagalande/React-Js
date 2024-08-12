5507ccac7d7d015a81df7192ea2d555555de3f90
Aditya Galande <56816119+adityagalande@users.noreply.github.com>
2024-08-12 20:57:57 +0000

    sdsu

--- 14MegaBlogAppwrite/src/components/index.js
+++ 14MegaBlogAppwrite/src/components/index.js
@@ -3,11 +3,22 @@ import Footer from './footer/Footer'
 import Container from './container/Container'
 import LogoutBtn from './header/LogoutBtn'
 import Logo from './Logo'
+import Button from './Button'
+import Select from './Select'
+import PostCard from './PostCard'
+import Input from './Input'
+import Login from './Login'
+
 
 export {
     Header,
     Footer,
     Container,
     LogoutBtn,
-    Logo
+    Logo,
+    Button,
+    Select,
+    PostCard,
+    Input,
+    Login,
 }
